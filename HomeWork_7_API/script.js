227ffb06bbb30ce34ffc584f601a205baaa9b176
Juri_Buerkle <rastopka@gmx.de>
2025-07-13 20:14:28 +0200

    add

--- HomeWork_7_API/script.js
+++ HomeWork_7_API/script.js
@@ -1,5 +1,5 @@
-fetch("https://juribuerkle.github.io/my_favorite_Cars/autos.json") 
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  });
+fetch("https://juribuerkle.github.io/my_favorite_Cars/") 
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });
