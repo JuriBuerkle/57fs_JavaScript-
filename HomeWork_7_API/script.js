68114e0dac18d1d5260af9dd4ead96fce8ccd40f
Juri_Buerkle <rastopka@gmx.de>
2025-07-13 09:34:44 +0200

    add

--- HomeWork_7_API/script.js
+++ HomeWork_7_API/script.js
@@ -1,18 +1,5 @@
-fetch("https://juribuerkle.github.io/my_favorite_Cars/cars.json")
-    .then(response => response.json())
-    .then(data => {
-        const carList = document.getElementById("car-list");
-        data.cars.forEach(car => {
-            const carItem = document.createElement("div");
-            carItem.classList.add("car-item");
-            carItem.innerHTML = `
-                <h2>${car.brand} ${car.model}</h2>
-                <p>Year: ${car.year}</p>
-                <p>Interior Color: ${car.color.interior}</p>
-                <p>Car Color: ${car.color.car}</p>
-                <img src="${car.image}" alt="${car.brand} ${car.model}">
-            `;
-            carList.appendChild(carItem);
-        });
-    })
-    .catch(error => console.error("Error fetching car data:", error));
+fetch("https://juribuerkle.github.io/my_favorite_Cars/autos.json") 
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  });
